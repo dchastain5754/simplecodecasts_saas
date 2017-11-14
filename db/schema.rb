312cbf4b7f95a0ef5ff294fc925d0523099362f6
Derek Chastain <dchastain5754@gmail.com>
2017-11-14 07:15:25 +0000

    fixed spelling error in schema file

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20171114063816) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_id"
-    t.string   "stripe_customers_token"
+    t.string   "stripe_customer_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
